84e7fdd0511222b65c259e5f4aa1c311ae14996a
Umar Zainutdinov <Ilnur16REGGY@gmail.com>
2025-04-23 20:08:31 +0700

    Добавил путь до файла приложением

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -1,5 +1,11 @@
+import java.nio.file.Paths
+
 object Constants {
     const val DIGITAL_ONLY_PRODUCT = "Digital-only"
     const val SUCCESS_ORDER_NOTIFICATION = "Woohoo! Thank you for your purchase."
     const val DECLINE_PAYMENT_NOTIFICATION = "Your card was declined."
+    private const val APK_FOLDER = "src/test/resources/"
+    private const val APK_FILE = "app.apk"
+
+    val APP_PATH = Paths.get(APK_FOLDER, APK_FILE).toAbsolutePath().toString()
 }
